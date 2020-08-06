e167b17669769c664d27117f9bda4add445d9696
unknown <jks961616@naver.com>
2020-08-06 16:32:31 +0900

    upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public GameObject readyPanel;
+    public Text scoreText;
+    public Text bestScoreText;
+    public Text messageText;
+    public bool isRoundActive = false;
+    private int score = 0;
+    public ShooterRotator shooterRotator;
+    public CamFollow cam;
+    public UnityEvent onReset;
+
+    void Awake()
+    {
+        instance = this;
+        UpdateUI();
+    }
+
+    void Start()
+    {
+        StartCoroutine("RoundRoutine");
+    }
+
+    public void AddScore(int newScore)
+    {
+        score += newScore;
+        UpdateBestScore();
+        UpdateUI();
+    }
+
+    void UpdateBestScore()
+    {
+        if(GetBestScore() < score) 
+            PlayerPrefs.SetInt("BestScore", score);
+    }
+
+    int GetBestScore()
+    {
+        int bestScore = PlayerPrefs.GetInt("BestScore");
+        return bestScore;
+    }
+
+    void UpdateUI()
+    {
+        scoreText.text = "Score : " + score;
+        bestScoreText.text = "Best Score : " + GetBestScore();
+    }
+
+    public void OnBallDestroy()
+    {
+        UpdateUI();
+        isRoundActive = false;
+    }
+
+    public void Reset()
+    {
+        score = 0;
+        UpdateUI();
+
+        // 라운드를 처음부터 다시 시작
+        StartCoroutine("RoundRoutine");
+    }
+
+    IEnumerator RoundRoutine()
+    {
+        // Ready
+        onReset.Invoke();
+
+        readyPanel.SetActive(true);
+        cam.SetTarget(shooterRotator.transform, CamFollow.State.Idle);
+        shooterRotator.enabled = false;
+
+        isRoundActive = false;
+
+        messageText.text = "Ready...";
+
+        yield return new WaitForSeconds(3f);
+
+        // Play
+
+        isRoundActive = true;
+        readyPanel.SetActive(false);
+        shooterRotator.enabled = true;
+
+        cam.SetTarget(shooterRotator.transform, CamFollow.State.Ready);
+
+        while(isRoundActive == true)
+        {
+            yield return null;
+        }
+
+        // End
+
+        readyPanel.SetActive(true);
+        shooterRotator.enabled = false;
+
+        messageText.text = "Wait For Next Round...";
+
+        yield return new WaitForSeconds(3f);
+
+        Reset();
+    }
+}
---
+++ Assets/Scripts/SpawnGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGenerator : MonoBehaviour
+{
+    private BoxCollider area;
+    public GameObject[] propPrefabs;
+    public int count = 100;
+
+    private List<GameObject> props = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        area = GetComponent<BoxCollider>();
+
+        for(int i = 0; i < 100; i++)
+        {
+            Spawn();
+        }
+
+        area.enabled = false;
+    }
+
+    private void Spawn()
+    {
+        int selection = Random.Range(0, propPrefabs.Length);
+        GameObject selectedPrefab = propPrefabs[selection];
+        Vector3 spawnPos = GetRandomPosition();
+
+        GameObject instance = Instantiate(selectedPrefab, spawnPos, Quaternion.identity);
+        props.Add(instance);
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        Vector3 basePosition = transform.position;
+        Vector3 size = area.size;
+
+        float posX = basePosition.x + Random.Range(-size.x / 2, size.x / 2);
+        float posY = basePosition.y + Random.Range(-size.y / 2, size.y / 2);
+        float posZ = basePosition.z + Random.Range(-size.z / 2, size.z / 2);
+
+        Vector3 spawnPos = new Vector3(posX, posY, posZ);
+
+        return spawnPos;
+    }
+
+    public void Reset()
+    {
+        for(int i = 0; i < props.Count; i++)
+        {
+            props[i].transform.position = GetRandomPosition();
+            props[i].SetActive(true);
+        }
+    }
+}
